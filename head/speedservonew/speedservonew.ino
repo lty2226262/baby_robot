42ebdd9992b18637bc4ba321b405bb2eb859905a
Joey.T.Y.Liu <lty2226262@gmail.com>
2017-06-07 12:44:54 +0800

    smooth speed
    smooth speed

--- head/speedservonew/speedservonew.ino
+++ head/speedservonew/speedservonew.ino
@@ -1,4 +1,5 @@
 #include <Servo.h>
+#define ROTARION_RANGE 180
 
 
 Servo myservoH_, myservoVlr_, myservoVfb_;
@@ -7,15 +8,25 @@ Servo myServos[3] = {myservoH_, myservoVlr_, myservoVfb_};
 int currentAngles[3] = {90, 108, 90}; // 三个舵机, 头水平, 头俯仰, 脸蛋
 
 const int initDelayTime = 30; //舵机初始化延时时间
-const int moveDelayTime = 10; //舵机运动延时时间 最快为4，最慢是100
+const int moveDelayTime = 10; //舵机运动延时时间 最快为4，最慢是10
+const int moveDelayMin = 4;
+const int moveDelayMax = 100;
 const int powerDelay = 1000; //运动完成后延时断电
 const float pParament = 4.0; //PID control >0
 const int trigPin = 8;
 const int servoZeroPin = 9;
-byte incomingByte[2];
-unsigned char sign1=0;
-int i = 0;
 const int resolution = 1;
+const float weight_smooth = 0.1;
+const float tolerance = 0.00001;
+const float accelerate_limit = 0.5;
+
+byte incomingByte[2];
+unsigned char sign1 = 0;
+int i = 0;
+float new_path[ROTARION_RANGE];
+int flexible[ROTARION_RANGE];
+
+
 //float x[180];
 //float y[180];
 //int currentangleH=90;//三个舵机 头水平
@@ -24,46 +35,102 @@ const int resolution = 1;
 
 
 
-float ComputeParament(float parament, int targetAngle, int k, int currentAngle){
-  
-  int scala = abs(currentAngle - targetAngle);
-  if (targetAngle >= k){
-    parament = 1- (targetAngle - k) / scala;
-  } else {
-    parament = 1- (k - targetAngle) / scala;
+// float ComputeParament(float parament, int targetAngle, int k, int currentAngle){
+//
+//   int scala = abs(currentAngle - targetAngle);
+//   if (targetAngle >= k){
+//     parament = 1- (targetAngle - k) / scala;
+//   } else {
+//     parament = 1- (k - targetAngle) / scala;
+//   }
+//   if (parament < 0.5) parament = 1 - parament;
+//   parament -= 1.0;
+//   parament *= pParament;
+//   parament += 1.0;
+//
+//   if (parament < (float)moveDelayMin /moveDelayTime ) parament = moveDelayMin/moveDelayTime;
+//   if (parament > (float)moveDelayMax /moveDelayTime) parament = moveDelayMax/moveDelayTime;
+//   return parament;
+// }
+
+void PathInitialize(){
+  for( int i = 0; i < ROTARION_RANGE; i++){
+    new_path[i] = 0;
+    flexible[i] = 0;
   }
-  if (parament < 0.5) parament = 1 - parament;
-  parament -= 1.0;
-  parament *= pParament;
-  parament += 1.0;
-  
-  if (parament < (float)4/moveDelayTime ) parament = 4/moveDelayTime;
-  if (parament > (float)100/moveDelayTime) parament = 100/moveDelayTime;
-  return parament;
+}
+
+void Smooth(){
+  float error = tolerance;
+  float aux;
+  while (error >= tolerance){
+    error = 0;
+    for (int i = 0; i < ROTARION_RANGE; i++){
+      if (flexible[i] == 1){
+        aux = new_path[i];
+        new_path[i] += (weight_smooth * (new_path[i - 1] + new_path[i + 1] - 2 * new_path[i]) + (weight_smooth / 2) * (2 * new_path[i - 1] - new_path[i - 2] - new_path[i]) + (weight_smooth / 2) * (2 * new_path[i + 1] - new_path[i + 2] - new_path[i]));
+        error += abs(new_path[i] - aux);
+      }
+    }
+  }
+}
+
+void ComputeParameter(int targetAngle, int currentAngle){
+  PathInitialize();
+  int scale = abs(currentAngle - targetAngle);
+  for (int i = 0; i < scale; i++){
+    new_path[i] = 1;
+  }
+  for (int i = 2; i < scale - 2; i++){
+    flexible[i] = 1;
+  }
+  int mid_index = (int)(scale / 2);
+  float mid_value = 1 - (float)mid_index * accelerate_limit / moveDelayTime;
+  if (mid_value < (float)moveDelayMin / moveDelayTime){
+    mid_value = (float)moveDelayMin / moveDelayTime;
+  }
+  new_path[mid_index] = mid_value;
+  flexible[mid_index] = 0;
+  Smooth;
 }
 
 void servoRun(int targetAngle, int servoNum){
   float parament;
   setTrig(HIGH);//HIGH
   myServos[servoNum].attach(servoNum + servoZeroPin,900,2100);
+  // if( (targetAngle-currentAngles[servoNum] )>0){
+  //   for(int k=currentAngles[servoNum]; k <= targetAngle; ){
+  //     k += resolution;
+  //     //k += (int)(pParament * ( targetAngle - k));
+  //     myServos[servoNum].write(k);
+  //     parament = ComputeParament(parament, targetAngle, k, currentAngles[servoNum]);
+  //     delay((int)(parament * moveDelayTime));
+  //   }
+  // } else {
+  //   for(int k=currentAngles[servoNum];k >= targetAngle;){
+  //     k -= resolution;
+  //     //k += (int)(pParament * ( targetAngle - k));
+  //     myServos[servoNum].write(k);
+  //     parament = ComputeParament(parament, targetAngle, k, currentAngles[servoNum]);
+  //     delay((int)(parament * moveDelayTime));
+  //   }
+  // }
+  // //myServos[servoNum].detach();
+  ComputeParameter(targetAngle, currentAngles[servoNum]);
   if( (targetAngle-currentAngles[servoNum] )>0){
     for(int k=currentAngles[servoNum]; k <= targetAngle; ){
-      k += resolution;
-      //k += (int)(pParament * ( targetAngle - k));
-      myServos[servoNum].write(k);
-      parament = ComputeParament(parament, targetAngle, k, currentAngles[servoNum]);
-      delay((int)(parament * moveDelayTime));
+        k++;
+        myServos[servoNum].write(k);
+        delay((int)(moveDelayTime * new_path[k - 1 - currentAngles[servoNum]]));
     }
-  } else {
-    for(int k=currentAngles[servoNum];k >= targetAngle;){
-      k -= resolution;
-      //k += (int)(pParament * ( targetAngle - k));
-      myServos[servoNum].write(k);
-      parament = ComputeParament(parament, targetAngle, k, currentAngles[servoNum]);
-      delay((int)(parament * moveDelayTime));
+  } else{
+    for(int k=currentAngles[servoNum]; k >= targetAngle; ){
+        k--;
+        myServos[servoNum].write(k);
+        delay((int)(moveDelayTime * new_path[currentAngles[servoNum] - k + 1]));
     }
   }
-  //myServos[servoNum].detach();
+
   delay(powerDelay);
   setTrig(LOW);//LOW
 }
@@ -78,7 +145,7 @@ void setup() {
   pinMode(trigPin,OUTPUT);
   setTrig(LOW);//LOW
   for (int i = 0; i < 3;i++){
-    
+
     servoRun(currentAngles[i],i);
     delay(initDelayTime);
   }
@@ -119,3 +186,4 @@ void loop() {
     }
   }
 }
+
